ac4cf7d122fc576c7add10aa8c07b21f05083c33
BlumaSavrasov <45488076+BlumaSavrasov@users.noreply.github.com>
2020-05-17 16:27:42 +0300

    Add files via upload

---
+++ WpfApp1/Models/ManageFallReportModel.cs
@@ -0,0 +1,38 @@
+﻿using BE;
+using BL;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PL.Models
+{
+    public class ManageFallReportModel
+    {
+        readonly IBL ibl = FactoryBL.GetBL();
+        public ManageFallReportModel()
+        {
+        }
+
+        public void RemoveFallReport(int idToDelete)
+        {
+            ibl.RemoveReport(idToDelete);
+        }
+        public void AddFallReport(Report fallReport)
+        {
+            ibl.AddReport(fallReport);
+        }
+        public ObservableCollection<Report> AllFallReports()
+        {
+            List<Report> temp = ibl.GetAllReports().ToList();
+            ObservableCollection<Report> theCollection = new ObservableCollection<Report>();
+            foreach (Report item in temp)
+                theCollection.Add(item);
+            return theCollection;
+
+        }
+    }
+}
---
+++ WpfApp1/Models/ManagePredictionReportAndRealFallsModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BE;
+using BL;
+using sun.awt;
+using static BE.Location_;
+
+namespace PL.Models
+{
+    public sealed class ManagePredictionReportAndRealFallsModel
+    {
+        readonly IBL ibl = FactoryBL.GetBL();
+        public List<Location_> ReportLocation { get; set; }
+        public List<Location_> FallLocation { get; set; }
+        public List<Location_> AssessmentLocation { get; set; }
+
+        private static ManagePredictionReportAndRealFallsModel instance = null;
+        public static ManagePredictionReportAndRealFallsModel Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new ManagePredictionReportAndRealFallsModel();
+                }
+                return new ManagePredictionReportAndRealFallsModel();
+            }
+        }
+
+        private ManagePredictionReportAndRealFallsModel()
+        {
+            ReportLocation = new List<Location_>();
+            FallLocation = new List<Location_>();
+            AssessmentLocation = new List<Location_>();
+            
+            FallLocation = (from fall in ibl.GetAllFalls()
+                            select fall.location).ToList();
+            foreach(Assessment a in ibl.GetAllAassessments().ToList())
+            {
+                foreach(Location_ l in a.locations)
+                {
+                    AssessmentLocation.Add(l);
+                }
+            }
+
+        
+
+
+            ReportLocation = (from r in ibl.GetAllReports()
+                              select r.location).ToList();
+        }
+    }
+}
